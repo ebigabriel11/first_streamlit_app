e21e93b52f191a99a93ec494ca159e5ca6b48287
dongabbyda <76711699+dongabbyda@users.noreply.github.com>
2022-04-22 21:21:33 +0100

    Update streamlit_app.py
    changes to breakfast favourite quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('My Mom\'s New Healthy Diner')
 
-st.header(''Breakfast Favorites'')
+st.header('Breakfast Favorites')
 
 breakfast= ['🥣 Omega 3 & Blueberry Oatmeal', '🥗 Kale, Spinach & Rocket Smoothie', '🐔 Hard-Boiled Free_range Egg','🥑🍞 Avocado Toast']
 for meal in breakfast:
