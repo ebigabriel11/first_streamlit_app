09e414b070aa476d80c8a2d701cb9c07a0d20769
dongabbyda <76711699+dongabbyda@users.noreply.github.com>
2022-04-22 21:26:39 +0100

    Update streamlit_app.py
    Added the pandas package to aid dataframe build

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title('My Mom\'s New Healthy Diner')
 
@@ -9,3 +10,6 @@ for meal in breakfast:
   st.text(meal)
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+st.dataframe(my_fruit_list)
