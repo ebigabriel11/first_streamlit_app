6687df30976e658cfbbbe8d1ce4eeea6d3c11e4d
dongabbyda <76711699+dongabbyda@users.noreply.github.com>
2022-04-22 21:01:22 +0100

    Update streamlit_app.py
    Updating the title info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title('My Parents New Healthy Diner')
+st.title('My Mom\'s New Healthy Diner')
+
+st.title('Breakfast Menu')
+
+st.write('Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free_range Egg')
